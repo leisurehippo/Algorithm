76564abc9f7d4be443641db39cfbde0ebda9dfae
wangjing <974292414@qq.com>
2017-03-27 23:09:57 +0800

    add commit

--- uva/chapter6/src/IdealPath.java
+++ uva/chapter6/src/IdealPath.java
@@ -2,43 +2,54 @@ import java.util.*;
 import java.util.concurrent.LinkedBlockingQueue;
 
 public class IdealPath {
+    //store the path between two rooms, if there is a path between room i and j, then graph[i][j] = 1
     public static int [][]graph;
+    //store the color between two rooms
     public static int [][]color;
+    //store the shortest path to the end room for each room
     public static int []distance;
+    //store the ideal color sequence for each room
     public static String []color_seq;
-    public static Map<Integer,ArrayList<Integer>> map = new HashMap<>();
+    //use for bfs
     public static Queue<Integer> queue = new LinkedBlockingQueue<Integer>();
+
+    /**
+     * Initialize parameters
+     * @param n
+     */
     public static void init(int n){
         graph = new int[n][n];
         color = new int[n][n];
         distance = new int[n];
         color_seq = new String[n];
-        map.clear();
         queue.clear();
     }
 
+    /**
+     * use bfs to find the ideal path
+     * @param n
+     */
     public static void bfs(int n){
+        //init the end room
         distance[n-1] = 0;
         color_seq[n-1] = "";
         queue.add(n-1);
+        //loop until the queue is empty
         while(!queue.isEmpty()){
             int node = queue.poll();
-            for (int i = 0; i < n-1; i++) {
+            for (int i = 0; i < n; i++) {
+                //there is a path
                 if (graph[node][i] == 1){
+                    //the room is not visited or is visited by the last floor
                     if (distance[i] == 0 || distance[i] == distance[node] + 1) {
-                        distance[i] = distance[node] + 1;
-                        String str = color[i][node] + " " + color_seq[node];
+                        distance[i] = distance[node] + 1;  //add the distance
+                        String str = color[i][node] + " " + color_seq[node];  //generate the color sequence
                         if (color_seq[i] == null)
                             color_seq[i] = str.trim();
+                            //choose the ideal color sequence for the room
                         else
                             color_seq[i] = compare(color_seq[i], str) ? str.trim() : color_seq[i].trim();
-                        if (!queue.contains(i))
-                            queue.add(i);
-//                        ArrayList<Integer> arrayList = new ArrayList<>();
-//                        if (map.containsKey(distance[i]))
-//                            arrayList = map.get(distance[i]);
-//                        arrayList.add(i);
-//                        map.put(distance[i],arrayList);
+                        queue.add(i);
                     }
                 }
             }
@@ -54,7 +65,7 @@ public class IdealPath {
     public static boolean compare(String str1, String str2){
         String []arr2 = str2.trim().split(" ");
         String []arr1 = str1.trim().split(" ");
-//        int n = Math.min(arr1.length,arr2.length);
+        //assume the lengths of arr1 and arr2 are same
         for (int i = 0; i < arr1.length; i++) {
             if (arr2[i].compareTo(arr1[i]) < 0)
                 return true;
@@ -62,49 +73,31 @@ public class IdealPath {
         return  false;
     }
 
-    public static String path(){
-        int dis = distance[0];
-        int node = 0;
-        String res = "";
-        while(dis != 0){
-            dis --;
-            ArrayList<Integer> arrayList = map.get(dis);
-            String str = color_seq[arrayList.get(0)];
-            int next = arrayList.get(0);
-            for (int i = 1; i < arrayList.size(); i++) {
-                String temp = color_seq[arrayList.get(i)];
-                if (compare(str,temp)){
-                    str = temp;
-                    next = arrayList.get(i);
-                }
-            }
-            res += " " + color[node][next];
-            node = next;
-        }
-        return res.substring(1);
-    }
-
     public static void main(String []args){
         Scanner input = new Scanner(System.in);
         while(input.hasNext()){
+            //input
             int n = input.nextInt();
             int m = input.nextInt();
             init(n);
             for (int i = 0; i < m; i++) {
                 int node1 = input.nextInt() - 1;
                 int node2 = input.nextInt() - 1;
-                if (node1 != node2){
+                //ignore the path to itself
+                if (node1 != node2) {
                     graph[node1][node2] = graph[node2][node1] = 1;
                     int c = input.nextInt();
-                    color[node1][node2] = color[node2][node1] = (color[node1][node2] == 0) ? c : Math.min(c,color[node1][node2]);
+                    //choose the smallest path if it has multi path
+                    color[node1][node2] = color[node2][node1] = (color[node1][node2] == 0) ? c : Math.min(c, color[node1][node2]);
                 }
             }
+            //use bfs to find the ideal path
             bfs(n);
-//            String res = path();
+            //output
+            //the ideal path is stored in the color_seq[0]
             String res = color_seq[0].trim();
             System.out.println((res.length() + 1) / 2);
             System.out.println(res);
         }
     }
 }
-
