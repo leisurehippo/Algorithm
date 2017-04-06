fe0767ba6f31aa2799b792380dfd14c4d1adfebf
WJ <WJ>
2017-04-06 17:15:52 +0800

    patrolrobot

--- uva/chapter6/src/Example/RecoveryOfFibonacci.java
+++ uva/chapter6/src/Example/RecoveryOfFibonacci.java
@@ -1,58 +1,58 @@
-package Example;
-
-/**
- * Created by H-E-Z on 2017/3/14.
- */
-class Node{
-    int id;
-    Example.Node[]next = new Example.Node[10];
-    Node(){
-        this.id = -1;
-        for (int i = 0; i < 10; i++) {
-            this.next[i] = null;
-        }
-    }
-}
-
-public class RecoveryOfFibonacci {
-    public static Example.Node root = new Example.Node();
-    public static int [][]F = new int[2][21000];
-    public static char []FF = new char[40];
-
-    public static void insert(char []number, int id, int len){
-        Example.Node node = root;
-        for (int i = 0; i < len; i++) {
-            char num = number[i];
-            int a = (int)num - 48;  //convert char to int
-            if (node.next[a] == null)
-                node.next[a] = new Example.Node();
-            node = node.next[a];
-        }
-        if (node.id == -1)
-            node.id = id;
-
-    }
-    public static void main(String[] args) {
-        F[0][0] = F[1][0] = 1;
-        FF[0] = '1';
-        insert(FF,0,1);
-        int start = 0, end = 1;
-        for (int i = 2; i < 100000; i++) {
-            int first = i % 2 ,second = (i+1) % 2;
-            for (int j = start; j < end; j++) {
-                F[first][j] = F[first][j] + F[second][j];
-            }
-        }
-
-
-        int a=1;
-//        Scanner input = new Scanner(System.in);
-//        int t = Integer.valueOf(input.nextLine());
-//        for (int i = 0; i < t; i++) {
-//            String number = input.nextLine();
+//package Example;
 //
-//
+///**
+// * Created by H-E-Z on 2017/3/14.
+// */
+//class Node{
+//    int id;
+//    Example.Node[]next = new Example.Node[10];
+//    Node(){
+//        this.id = -1;
+//        for (int i = 0; i < 10; i++) {
+//            this.next[i] = null;
 //        }
-
-    }
-}
+//    }
+//}
+//
+//public class RecoveryOfFibonacci {
+//    public static Example.Node root = new Example.Node();
+//    public static int [][]F = new int[2][21000];
+//    public static char []FF = new char[40];
+//
+//    public static void insert(char []number, int id, int len){
+//        Example.Node node = root;
+//        for (int i = 0; i < len; i++) {
+//            char num = number[i];
+//            int a = (int)num - 48;  //convert char to int
+//            if (node.next[a] == null)
+//                node.next[a] = new Example.Node();
+//            node = node.next[a];
+//        }
+//        if (node.id == -1)
+//            node.id = id;
+//
+//    }
+//    public static void main(String[] args) {
+//        F[0][0] = F[1][0] = 1;
+//        FF[0] = '1';
+//        insert(FF,0,1);
+//        int start = 0, end = 1;
+//        for (int i = 2; i < 100000; i++) {
+//            int first = i % 2 ,second = (i+1) % 2;
+//            for (int j = start; j < end; j++) {
+//                F[first][j] = F[first][j] + F[second][j];
+//            }
+//        }
+//
+//
+//        int a=1;
+////        Scanner input = new Scanner(System.in);
+////        int t = Integer.valueOf(input.nextLine());
+////        for (int i = 0; i < t; i++) {
+////            String number = input.nextLine();
+////
+////
+////        }
+//
+//    }
+//}
--- uva/chapter6/src/Exercise/PatrolRobot.java
+++ uva/chapter6/src/Exercise/PatrolRobot.java
@@ -2,6 +2,7 @@ package Exercise;
 
 import java.util.Queue;
 import java.util.Scanner;
+import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * Created by wj on 17/4/5.
@@ -15,29 +16,39 @@ class Grid{
         this.y = y;
         this.obstacle = obstacle;
     }
+
+    public Grid(int x, int y) {
+        this.x = x;
+        this.y = y;
+        this.obstacle = 0;
+    }
 }
 public class PatrolRobot {
     public static int m, n, k;
     public static int [][]graph;
     public static int [][]dis;
+    public static int [][]obstacle;
+    public static boolean [][]flag;
     public static int []move_x = {0, 1, 0, -1};
     public static int []move_y = {1, 0, -1, 0};
-    public static Queue<Grid> queue;
+    public static Queue<Grid> queue = new LinkedBlockingQueue<>();
 
     public static void init(int m ,int n){
         graph = new int[m][n];
         dis = new int[m][n];
+        obstacle = new int[m][n];
+        flag = new boolean[m][n];
         queue.clear();
     }
 
     public static Grid move(Grid grid, int i, int obstacle){
         int next_x = grid.x + move_x[i];
         int next_y = grid.y + move_y[i];
-        if (graph[next_x][next_y] == 1)
-            obstacle++;
-        else
-            obstacle = 0;
-        Grid res = new Grid(next_x, next_y, obstacle);
+        Grid res = new Grid(next_x, next_y);
+
+        if (is_inside(res) && graph[next_x][next_y] == 1)
+            res.obstacle = obstacle + 1;
+
         return res;
     }
 
@@ -46,7 +57,8 @@ public class PatrolRobot {
     }
 
     public static void bfs(){
-        dis[0][0] = 0;
+        dis[0][0] = 1;
+        obstacle[0][0] = 0;
         Grid start = new Grid(0,0,0);
         queue.add(start);
         while (!queue.isEmpty()){
@@ -54,7 +66,17 @@ public class PatrolRobot {
             for (int i = 0; i < 4; i++) {
                 Grid next = move(grid,i,grid.obstacle);
                 //TODO: select those grids which have less obstacle
-                if (is_inside(next) && next.obstacle <= k){
+                if (is_inside(next) && next.obstacle <= k && (dis[next.x][next.y] == 0 || dis[next.x][next.y] == dis[grid.x][grid.y] + 1)){
+
+                    if (!flag[next.x][next.y])
+                        obstacle[next.x][next.y] = next.obstacle;
+                    else{
+                        if (next.obstacle >= obstacle[next.x][next.y])
+                            next.obstacle = obstacle[next.x][next.y];
+                        else
+                            obstacle[next.x][next.y] = next.obstacle;
+                    }
+
                     dis[next.x][next.y] = dis[grid.x][grid.y] + 1;
                     queue.add(next);
                 }
@@ -66,16 +88,40 @@ public class PatrolRobot {
         Scanner input = new Scanner(System.in);
         int cases = input.nextInt();
         for (int i = 0; i < cases; i++) {
+
             m = input.nextInt();
             n = input.nextInt();
             k = input.nextInt();
+            init(m, n);
             for (int j = 0; j < m; j++) {
                 for (int l = 0; l < n; l++) {
                     int aij = input.nextInt();
                     graph[j][l] = aij;
                 }
             }
-            bfs();
+//            if (k > Math.max(n,m)){
+//                System.out.println(n + m - 2);
+//            }eles{
+//                bfs();
+//                System.out.println(dis[m-1][n-1] - 1);
+//            }
+            if (i == 298){
+                bfs();
+                for (int j = 0; j < m; j++) {
+                    for (int l = 0; l < n; l++) {
+                        System.out.print(dis[j][l]+" ");
+                    }
+                    System.out.println();
+                }
+                System.out.println();
+                for (int j = 0; j < m; j++) {
+                    for (int l = 0; l < n; l++) {
+                        System.out.print(obstacle[j][l]+" ");
+                    }
+                    System.out.println();
+                }
+            }
+
 
         }
     }
